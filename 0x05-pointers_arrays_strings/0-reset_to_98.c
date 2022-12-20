545c90b7e30d98885e1be224a498832181294a0c
johncyril53@gmail.com <johncyril53@gmail.com>
2022-12-19 16:14:44 -0800

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ *
+ * and updates the value it points to as 98
+ * @n: this is an input integer
+ * Return: Blank.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - This function prints a string, followed by a new line, to stdout.
+ *
+ * @str: Pointer argument of the function.
+ * Return: Nothing.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
