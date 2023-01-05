66f3a7a0d743ad443c3a2b34f14daab31dc27373
johncyril53@gmail.com <johncyril53@gmail.com>
2023-01-04 20:12:43 -0800

    fixer upper

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * _puts_recursion - entry point
  * Description - function that prints a string followed by a new line
- * @s: pointer to string to be printed
+*  * @s: pointer to string to be printed
  * Return: 0
- */
+  */
 void _puts_recursion(char *s)
 {
 	if (*s)
